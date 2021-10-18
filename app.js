edb5460131059796eb911807892b13aba4ed670a
Jeff Wu <32234574+jeffwu0724@users.noreply.github.com>
2021-10-18 09:44:57 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mailchimp = require("@mailchimp/mailchimp_marketing");
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.use(express.static("html"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/signup", function(req, res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  console.log(firstName, lastName, email);
+  var data = {
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+    ]
+  };
+  // const jonData = JSON.stringify(data);
+  // const url = "https://$API_SERVER.api.mailchimp.com/3.0/lists/417f414cd5";
+  // const options {
+  //   method: "POST",
+  // }
+  // https.request(url, options, function(response))
+
+  mailchimp.setConfig({
+   apiKey: "d59dc9b4342fb2de56fade924d945699-us5",
+   server: "us5",
+ });
+ async function run() {
+
+      const response = await mailchimp.lists.batchListMembers("417f414cd5", data);
+      console.log(response);
+      console.log(response.error_count);
+      if(response.error_count != 0){
+        res.sendFile(__dirname + "/html/failure.html");
+      }else{
+        res.sendFile(__dirname + "/html/success.html");
+      }
+
+  };
+  run();
+
+
+});
+
+app.post("/failure", function(req, res) {
+  res.sendFile(__dirname + "/html/signup.html");
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on Port 3000");
+});
